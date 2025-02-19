3d6f7e9874ffaed33f612e3bc0d57ebb7b816036
Anthony Barragan <anthony.bar.89@gmail.com>
2025-02-18 18:18:53 -0800

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1>Larkspur & Lily</h1>
+      <Button>
+        Submit
+      </Button>
+    </div>
+  )
+}
