be9eaccebe3e8f600767ad19043d5b13d7611d00
Anthony Barragan <anthony.bar.89@gmail.com>
2025-02-21 23:13:49 -0800

    added csp in middleware

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from "@/lib/constants";
 import { ThemeProvider } from "next-themes";
+import { headers } from 'next/headers';
 import "./globals.css";
 
 const inter = Inter({ subsets: ['latin'] });
@@ -14,14 +15,20 @@ export const metadata: Metadata = {
   metadataBase: new URL(SERVER_URL)
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const headersList = await headers();
+  const nonce = headersList.get('x-nonce');
+  if (!nonce) return
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body
+        nonce={nonce}
         className={`${inter.className} antialiased`}
       >
         <ThemeProvider
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,49 @@
-// export { auth as middleware } from "@/lib/auth"
-export default function middlewarefun() {
-    console.log('hi')
+import { NextResponse } from 'next/server';
+
+export async function middleware() {
+    const nonce = Buffer.from(crypto.randomUUID()).toString('base64');
+
+    // Apply Content Security Policy (CSP)
+    let cspHeader = '';
+    if (process.env.NODE_ENV === 'production') {
+        cspHeader = `
+        default-src 'self';
+        script-src 'self' 'nonce-${nonce}' https://vercel.live/_next-live/feedback/;
+        style-src 'self' 'unsafe-inline';
+        img-src 'self' blob: data: https://authjs.dev/;
+        font-src 'self';
+        object-src 'none';
+        base-uri 'self';
+        form-action 'self';
+        frame-ancestors 'none';
+        worker-src 'self' blob:;
+        frame-src https://vercel.live/;
+        connect-src 'self' blob: https://www.google-analytics.com/ https://analytics.google.com/;
+        upgrade-insecure-requests;
+    `;
+    } else if (process.env.NODE_ENV === 'development') {
+    //     cspHeader = `
+    //     default-src 'self';
+    //     script-src 'self' 'unsafe-inline' 'nonce-${nonce}' 'unsafe-eval' http://localhost:3000 https://pagead2.googlesyndication.com/ https://cdn.jsdelivr.net https://www.googletagmanager.com/gtag/;
+    //     style-src 'self' 'unsafe-inline';
+    //     img-src 'self' blob: data: http://localhost:3000 https://unfinished-pages.s3.us-east-2.amazonaws.com https://*.googleusercontent.com https://*.yahoo.com https://*.outlook.com https://authjs.dev/ https://ep1.adtrafficquality.google/pagead/ https://pagead2.googlesyndication.com/;
+    //     font-src 'self';
+    //     object-src 'none';
+    //     worker-src 'self' blob:;
+    //     base-uri 'self';
+    //     form-action 'self';
+    //     frame-src https://www.youtube.com/ https://googleads.g.doubleclick.net/ https://ep2.adtrafficquality.google/ https://www.google.com/;
+    //     connect-src 'self' blob: https://ep1.adtrafficquality.google/ https://csi.gstatic.com/ https://www.googletagmanager.com/gtag/;
+    //     frame-ancestors 'none';
+    //     upgrade-insecure-requests;
+    // `;
+    }
+    
+    const response = NextResponse.next();
+
+    const contentSecurityPolicyHeaderValue = cspHeader.replace(/\s{2,}/g, ' ').trim();
+    response.headers.set('Content-Security-Policy', contentSecurityPolicyHeaderValue);
+    response.headers.set('x-nonce', nonce);
+
+    return response;
 }
